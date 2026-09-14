462bd1ca212ce70900f37a1aad8a60be9009cdc5
marcos314159 <marquitommelo@gmail.com>
2026-09-14 09:29:14 -0300

    Fix Controlled regression

--- src/Library/MathHelper.cs
+++ src/Library/MathHelper.cs
@@ -8,7 +8,7 @@ public class MathHelper
 {
     public bool IsPrime(int n)
     {
-        if (n > 2) return false;
+        if (n < 2) return false;
         if (n == 2) return true;
         if (n % 2 == 0) return false;
         for (int i = 3; i * i <= n; i += 2)
